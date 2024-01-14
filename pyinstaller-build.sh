e5437e9679cd7535446d1f7d7139550b0da44de9
ansonl <anson@ansonliu.com>
2024-01-13 19:47:59 -0800

    Add macos osx pyinstaller build notes

---
+++ pyinstaller-build.sh
@@ -0,0 +1,4 @@
+# MacOS
+# Create code signing identity first
+# https://github.com/pyinstaller/pyinstaller/issues/6167#issuecomment-906307356
+pyinstaller --onefile gui.py --codesign-identity ansonliu-imac-code-sign --name MFPP
