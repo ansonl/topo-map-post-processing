6c2a2ba59ecf96a25595cc5b00f3bd3cdb404a6d
Anson Liu <ansonl@users.noreply.github.com>
2024-05-09 15:49:41 -0700

    update gui.py path after reorg

--- pyinstaller-build.sh
+++ pyinstaller-build.sh
@@ -1,7 +1,7 @@
-# Window
-pyinstaller --onefile gui.py --name MFM
+# Windows
+pyinstaller --onefile src/gui.py --name MFM
 
 # MacOS
 # Create code signing identity first
 # https://github.com/pyinstaller/pyinstaller/issues/6167#issuecomment-906307356
-pyinstaller --onefile gui.py --codesign-identity ansonliu-imac-code-sign --name MFM
+pyinstaller --onefile src/gui.py --codesign-identity ansonliu-imac-code-sign --name MFM
